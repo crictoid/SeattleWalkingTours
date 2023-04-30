f8f497dbf154a1237de919396f13ee6e73cf0a36
Mark David Bulmer <mdbulmer86@gmail.com>
2023-04-30 18:23:58 -0400

    Add files via upload

---
+++ BulmerCPACh4No2/frmWalkingTour.Designer.vb
@@ -0,0 +1,187 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmWalkingTour
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnCost = New System.Windows.Forms.Button()
+        Me.lblTotalCost = New System.Windows.Forms.Label()
+        Me.lblTotalLabel = New System.Windows.Forms.Label()
+        Me.txtNumberOfDays = New System.Windows.Forms.TextBox()
+        Me.lblNumberOfDays = New System.Windows.Forms.Label()
+        Me.lblCostHeading = New System.Windows.Forms.Label()
+        Me.lblHeading = New System.Windows.Forms.Label()
+        Me.picAirbnb = New System.Windows.Forms.PictureBox()
+        CType(Me.picAirbnb, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.Silver
+        Me.btnExit.Font = New System.Drawing.Font("Courier New", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(256, 390)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(116, 48)
+        Me.btnExit.TabIndex = 29
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.Silver
+        Me.btnClear.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnClear.Font = New System.Drawing.Font("Courier New", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(134, 390)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(116, 48)
+        Me.btnClear.TabIndex = 28
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'btnCost
+        '
+        Me.btnCost.BackColor = System.Drawing.Color.Silver
+        Me.btnCost.Font = New System.Drawing.Font("Courier New", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCost.Location = New System.Drawing.Point(12, 390)
+        Me.btnCost.Name = "btnCost"
+        Me.btnCost.Size = New System.Drawing.Size(116, 48)
+        Me.btnCost.TabIndex = 27
+        Me.btnCost.Text = "Display Cost"
+        Me.btnCost.UseVisualStyleBackColor = False
+        '
+        'lblTotalCost
+        '
+        Me.lblTotalCost.AutoSize = True
+        Me.lblTotalCost.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotalCost.ForeColor = System.Drawing.Color.White
+        Me.lblTotalCost.Location = New System.Drawing.Point(223, 243)
+        Me.lblTotalCost.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.lblTotalCost.Name = "lblTotalCost"
+        Me.lblTotalCost.Size = New System.Drawing.Size(67, 19)
+        Me.lblTotalCost.TabIndex = 26
+        Me.lblTotalCost.Text = "$888.88"
+        '
+        'lblTotalLabel
+        '
+        Me.lblTotalLabel.AutoSize = True
+        Me.lblTotalLabel.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotalLabel.ForeColor = System.Drawing.Color.White
+        Me.lblTotalLabel.Location = New System.Drawing.Point(39, 243)
+        Me.lblTotalLabel.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.lblTotalLabel.Name = "lblTotalLabel"
+        Me.lblTotalLabel.Size = New System.Drawing.Size(157, 19)
+        Me.lblTotalLabel.TabIndex = 25
+        Me.lblTotalLabel.Text = "Total Cost of Tickets:"
+        '
+        'txtNumberOfDays
+        '
+        Me.txtNumberOfDays.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNumberOfDays.Location = New System.Drawing.Point(227, 188)
+        Me.txtNumberOfDays.Margin = New System.Windows.Forms.Padding(5)
+        Me.txtNumberOfDays.Name = "txtNumberOfDays"
+        Me.txtNumberOfDays.Size = New System.Drawing.Size(37, 27)
+        Me.txtNumberOfDays.TabIndex = 24
+        Me.txtNumberOfDays.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'lblNumberOfDays
+        '
+        Me.lblNumberOfDays.AutoSize = True
+        Me.lblNumberOfDays.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNumberOfDays.ForeColor = System.Drawing.Color.White
+        Me.lblNumberOfDays.Location = New System.Drawing.Point(39, 188)
+        Me.lblNumberOfDays.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.lblNumberOfDays.Name = "lblNumberOfDays"
+        Me.lblNumberOfDays.Size = New System.Drawing.Size(190, 19)
+        Me.lblNumberOfDays.TabIndex = 23
+        Me.lblNumberOfDays.Text = "Enter Number of Tickets:"
+        '
+        'lblCostHeading
+        '
+        Me.lblCostHeading.AutoSize = True
+        Me.lblCostHeading.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCostHeading.ForeColor = System.Drawing.Color.White
+        Me.lblCostHeading.Location = New System.Drawing.Point(76, 76)
+        Me.lblCostHeading.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.lblCostHeading.Name = "lblCostHeading"
+        Me.lblCostHeading.Size = New System.Drawing.Size(152, 20)
+        Me.lblCostHeading.TabIndex = 22
+        Me.lblCostHeading.Text = "$XX.XX per ticket"
+        '
+        'lblHeading
+        '
+        Me.lblHeading.AutoSize = True
+        Me.lblHeading.Font = New System.Drawing.Font("Cooper Black", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeading.ForeColor = System.Drawing.Color.White
+        Me.lblHeading.Location = New System.Drawing.Point(6, 9)
+        Me.lblHeading.Margin = New System.Windows.Forms.Padding(5, 0, 5, 0)
+        Me.lblHeading.Name = "lblHeading"
+        Me.lblHeading.Size = New System.Drawing.Size(373, 27)
+        Me.lblHeading.TabIndex = 21
+        Me.lblHeading.Text = "Seattle Walking Tour Tickets"
+        '
+        'picAirbnb
+        '
+        Me.picAirbnb.Image = Global.BulmerCPACh4No2.My.Resources.Resources.seattle
+        Me.picAirbnb.Location = New System.Drawing.Point(392, 0)
+        Me.picAirbnb.Margin = New System.Windows.Forms.Padding(5)
+        Me.picAirbnb.Name = "picAirbnb"
+        Me.picAirbnb.Size = New System.Drawing.Size(407, 452)
+        Me.picAirbnb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picAirbnb.TabIndex = 20
+        Me.picAirbnb.TabStop = False
+        '
+        'frmWalkingTour
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCost)
+        Me.Controls.Add(Me.lblTotalCost)
+        Me.Controls.Add(Me.lblTotalLabel)
+        Me.Controls.Add(Me.txtNumberOfDays)
+        Me.Controls.Add(Me.lblNumberOfDays)
+        Me.Controls.Add(Me.lblCostHeading)
+        Me.Controls.Add(Me.lblHeading)
+        Me.Controls.Add(Me.picAirbnb)
+        Me.Name = "frmWalkingTour"
+        Me.Text = "Seattle Walking Tour Tickets"
+        CType(Me.picAirbnb, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnCost As Button
+    Friend WithEvents lblTotalCost As Label
+    Friend WithEvents lblTotalLabel As Label
+    Friend WithEvents txtNumberOfDays As TextBox
+    Friend WithEvents lblNumberOfDays As Label
+    Friend WithEvents lblCostHeading As Label
+    Friend WithEvents lblHeading As Label
+    Friend WithEvents picAirbnb As PictureBox
+End Class
---
+++ BulmerCPACh4No2/frmWalkingTour.vb
@@ -0,0 +1,59 @@
+﻿' Program:  Seattle Walking Tours
+' Author:   Mark Bulmer
+' Date:     February 13, 2022
+' Purpose:  This application calculates and displays the total
+'           cost of buying Seattle walking tour tickets.
+
+Option Strict On
+
+Public Class frmWalkingTour
+
+    ' Cost per ticket - used in multiple procedures
+    Const _cdecPricePerDay As Decimal = 14.99D
+
+
+    Private Sub btnCost_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnCost.Click
+        ' This event handler is executed when the user clicks the
+        ' Find Cost button. It calculates and displays the cost
+        ' of the ticket purchased (number of tickets times the cost per ticket).
+
+        Dim strNumberOfDays As String
+        Dim intNumberOfDays As Integer
+        Dim decTotalCost As Decimal
+
+        strNumberOfDays = txtNumberOfDays.Text
+        intNumberOfDays = Convert.ToInt32(strNumberOfDays)
+        decTotalCost = intNumberOfDays * _cdecPricePerDay
+        lblTotalCost.Text = decTotalCost.ToString("C")
+    End Sub
+
+    Private Sub btnClear_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnClear.Click
+        ' This event handler is executed when the user clicks the
+        ' Clear button. It clears the number of days text box
+        ' and the Text property of the Total Cost label.
+        ' Then it sets the focus on the txtNumberOfDays Textbox object.
+
+        txtNumberOfDays.Clear()
+        lblTotalCost.Text = ""
+        txtNumberOfDays.Focus()
+
+    End Sub
+
+    Private Sub frmDigitalDownloads_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+        ' This event handler is executed when the form is loaded.
+        ' It displays the cost heading, clears the Text property of the
+        ' Total Cost label, and sets the focus on
+        '  the txtNumberOfDays Textbox object.
+        lblCostHeading.Text = _cdecPricePerDay.ToString("C") & " per ticket"
+        lblTotalCost.Text = ""
+        txtNumberOfDays.Focus()
+
+    End Sub
+
+    Private Sub btnExit_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnExit.Click
+        ' Close the window and terminate the application.
+
+        Close()
+    End Sub
+
+End Class
